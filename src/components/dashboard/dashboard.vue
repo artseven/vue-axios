fbc960b484fbd2f2576fd92a2a76b588f91a5c2c
artseven <yabkevich.artsiom.09@gmail.com>
2019-12-12 11:55:01 -0500

    fetching data from firebase

--- src/components/dashboard/dashboard.vue
+++ src/components/dashboard/dashboard.vue
@@ -2,15 +2,44 @@
   <div id="dashboard">
     <h1>That's the dashboard!</h1>
     <p>You should only get here if you're authenticated!</p>
+    <p>Your email address: {{ email }} </p>
   </div>
 </template>
 
-<style scoped>
-  h1, p {
-    text-align: center;
-  }
+<script>
+import axios from "axios";
 
-  p {
-    color: red;
+export default {
+  data() {
+    return {
+      email: ''
+    }
+  },
+  created() {
+    axios
+      .get("https://vue-axios-aff58.firebaseio.com/users.json")
+      .then(res => {
+        const data = res.data;
+        const users = [];
+        for (let key in data) {
+          const user = data[key]
+          user.id = key;
+          users.push(user)
+        }
+        console.log(users);
+        this.email = users[0].email;
+      })
+      .catch(err => console.error(err));
   }
+};
+</script>
+<style scoped>
+h1,
+p {
+  text-align: center;
+}
+
+p {
+  color: red;
+}
 </style>
